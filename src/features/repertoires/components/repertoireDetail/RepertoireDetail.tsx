f2704a1441b0eca8a1cdb628f54e3baa49ed4b96
anhlq1 <anhlq1@novaon.vn>
2023-02-07 15:33:53 +0700

    detail init data

--- src/features/repertoires/components/repertoireDetail/RepertoireDetail.tsx
+++ src/features/repertoires/components/repertoireDetail/RepertoireDetail.tsx
@@ -1,5 +1,25 @@
+import { useSelector } from 'react-redux';
 import style from './RepertoireDetail.module.scss';
+import { RootState, useAppDispatch } from '@store';
+import { useEffect } from 'react';
+import { fetchRepertoire } from '@features/repertoires/slice/repertoireSlice';
 
 export const RepertoireDetail = () => {
-  return <div className={style.detailWrapper}></div>;
+  const dispatch = useAppDispatch();
+  const { selectedRepertoireId, selectedRepertoire } = useSelector((state: RootState) => state.repertoire);
+
+  useEffect(() => {
+    if (selectedRepertoireId) {
+      dispatch(fetchRepertoire(selectedRepertoireId));
+    }
+  }, [selectedRepertoireId]);
+
+  if (!selectedRepertoireId || !selectedRepertoire) {
+    return <div></div>;
+  }
+  return (
+    <div className={style.detailWrapper}>
+      <div>{selectedRepertoire.name}</div>
+    </div>
+  );
 };
--- src/features/repertoires/slice/repertoireSlice.ts
+++ src/features/repertoires/slice/repertoireSlice.ts
@@ -7,6 +7,7 @@ export interface RepertoireState {
   data: IRepertoireItem[];
   loading: boolean;
   selectedRepertoireId?: string;
+  selectedRepertoire?: IRepertoireItem;
 }
 
 const initialState: RepertoireState = {
@@ -31,6 +32,15 @@ const initialState: RepertoireState = {
     },
   ],
   loading: false,
+  selectedRepertoire: {
+    id: '1',
+    name:' Tea For Two',
+    isDeleted: false,
+    pieceId: '1',
+    piece: {
+      name: 'Tea For Two',
+    },
+  }
 };
 
 export const fetchAllRepertoires = createAsyncThunk(
@@ -41,6 +51,21 @@ export const fetchAllRepertoires = createAsyncThunk(
       return;
     }
     return await baseService.get<IRepertoireItem[]>('/repertoires');
+  }
+);
+
+export const fetchRepertoire = createAsyncThunk(
+  'repertoire/fetchRepertoire',
+  async (_arg: string, { getState }) => {
+    const { loading } = getState() as RepertoireState;
+    if (loading) {
+      return;
+    }
+    return await baseService.get<IRepertoireItem>('/repertoire', {
+      params: {
+        id: _arg
+      }
+    });
   }
 );
 
@@ -65,6 +90,21 @@ export const repertoireSlice = createSlice({
         }
       )
       .addCase(fetchAllRepertoires.rejected.type, (state, action) => {
+        state.loading = false;
+      });
+
+    builder
+      .addCase(fetchRepertoire.pending.type, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(
+        fetchRepertoire.fulfilled.type,
+        (state, action: PayloadAction<IRepertoireItem>) => {
+          state.loading = false;
+          state.selectedRepertoire = action.payload;
+        }
+      )
+      .addCase(fetchRepertoire.rejected.type, (state, action) => {
         state.loading = false;
       });
   },
